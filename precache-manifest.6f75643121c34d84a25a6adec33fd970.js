8e026644219a4a73815b62dda950be9690afe3e0
“sangameshrently” <sangamesh.r@rently.com>
2020-06-18 00:04:07 +0530

    Updates

---
+++ precache-manifest.6f75643121c34d84a25a6adec33fd970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f386d4c2fd15349dece24412dc2739c3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "27c26948959ad1455115",
+    "url": "/my-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9a2019928fc40f49cce7",
+    "url": "/my-app/static/js/2.1d8f87f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.1d8f87f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c26948959ad1455115",
+    "url": "/my-app/static/js/main.35a78b5a.chunk.js"
+  },
+  {
+    "revision": "83fcc2e72d844357f5b6",
+    "url": "/my-app/static/js/runtime-main.e011cb1b.js"
+  }
+]);
